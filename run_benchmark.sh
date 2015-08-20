d0826b5ea9d2b2f93ca74d36dbb8a266d943f7dd
Asier Lacasta <alacasta@unizar.es>
2015-08-20 08:37:11 -0500

    Update run_benchmark.sh

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo 'Running GPU tests'
 ./GPU-SAC_gpu benchmark/ 8 >> benchmark/sac_gpu_times.out
 ./GPU-SAC_gpu benchmark/ 16 >> benchmark/sac_gpu_times.out
 ./GPU-SAC_gpu benchmark/ 32 >> benchmark/sac_gpu_times.out
@@ -6,6 +7,7 @@
 ./GPU-SAC_gpu benchmark/ 128 >> benchmark/sac_gpu_times.out
 ./GPU-SAC_gpu benchmark/ 256 >> benchmark/sac_gpu_times.out
 ./GPU-SAC_gpu benchmark/ 512 >> benchmark/sac_gpu_times.out
+echo 'Running CPU tests'
 ./GPU-SAC_cpu benchmark/ 8 >> benchmark/sac_cpu_times.out
 ./GPU-SAC_cpu benchmark/ 16 >> benchmark/sac_cpu_times.out
 ./GPU-SAC_cpu benchmark/ 32 >> benchmark/sac_cpu_times.out
